7bec1bb621adc567323f5d77c394cd838b4e6643
Vishal-dk <vishaldk758@gmail.com>
2025-05-25 17:54:51 +0530

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -13,14 +13,14 @@ const userSchema = new mongoose.Schema({
   }
 });
 
-// Hash password before saving
+
 userSchema.pre('save', async function(next) {
   if (!this.isModified('password')) return next();
   this.password = await bcrypt.hash(this.password, 10);
   next();
 });
 
-// Method to compare password on login
+
 userSchema.methods.comparePassword = function(candidatePassword) {
   return bcrypt.compare(candidatePassword, this.password);
 };
